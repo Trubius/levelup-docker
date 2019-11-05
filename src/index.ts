e65e4ce04dae54f24834c4f670fc53be9b966e5a
Gabor Trubel <gabor.trubel@codingsans.com>
2019-11-05 16:28:07 +0100

    fix(requestListener): fix response

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,12 @@ import http from 'http';
 const requestListener: http.RequestListener = (req, res) => {
   if (req.method === 'GET') {
     const body = Buffer.from('{"status":"ok"}');
-    return res
-      .writeHead(200, {
-        'Content-Length': Buffer.byteLength(body),
-        'Content-Type': 'application/json',
-      })
-      .end(body);
+    res.writeHead(200, {
+      'Content-Length': Buffer.byteLength(body),
+      'Content-Type': 'application/json',
+    });
+    res.end(body);
+    return res;
   }
 };
 
